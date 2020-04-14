735420595f5ec3779efb5c75b26572ee3dda8cce
Nils Vogels <n.vogels@aves-it.nl>
2020-04-14 13:29:52 +0200

    Add hcloud csi driver to basic install

--- variables.tf
+++ variables.tf
@@ -47,5 +47,6 @@ variable "addons_include" {
   default = [
     "https://raw.githubusercontent.com/kubernetes/dashboard/v1.10.1/src/deploy/recommended/kubernetes-dashboard.yaml",
     "https://gist.githubusercontent.com/superseb/499f2caa2637c404af41cfb7e5f4a938/raw/930841ac00653fdff8beca61dab9a20bb8983782/k8s-dashboard-user.yml",
+    "https://raw.githubusercontent.com/hetznercloud/csi-driver/v1.2.3/deploy/kubernetes/hcloud-csi.yml",
   ]
 }
