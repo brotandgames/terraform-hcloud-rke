d5cb39571e29c425003962b142b6a49cd2198627
brotandgames <brotandgames@gmail.com>
2020-05-04 19:11:34 +0200

    Add input/variable defaults to test.

--- test/main.tf
+++ test/main.tf
@@ -1,6 +1,15 @@
 module "k8s_cluster" {
   source = "../"
 
+  # Defaults
+  #
+  # addons_include = [
+  # "https://raw.githubusercontent.com/kubernetes/dashboard/v1.10.1/src/deploy/recommended/kubernetes-dashboard.yaml",
+  # "https://gist.githubusercontent.com/superseb/499f2caa2637c404af41cfb7e5f4a938/raw/930841ac00653fdff8beca61dab9a20bb8983782/k8s-dashboard-user.yml"
+  # ]
+  # docker_version = "18.06.2"
+  # image          = "ubuntu-16.04"
+
   ssh_private_key_path = "./.ssh/id_rsa"
   ssh_public_key_path  = "./.ssh/id_rsa.pub"
 
@@ -22,10 +31,10 @@ output "k8s_cluster_this" {
   value = module.k8s_cluster.this
 }
 
-###############################################################################
+
 # If you need kube_config_cluster.yml for using kubectl,
-# please uncomment follows.
-###############################################################################
+# please uncomment the following local_file resource.
+
 # resource "local_file" "kube_cluster_yaml" {
 #   filename          = "${path.root}/kube_config_cluster.yml"
 #   sensitive_content = module.k8s_cluster.kube_config_yaml
