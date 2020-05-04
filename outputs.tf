c546eb061a23251a61da9659e1cad93c13683d2c
WebSpider <WebSpider@users.noreply.github.com>
2020-05-04 13:01:01 +0200

    Add useful outputs for kubernetes provider authentication (#4)
    * Add useful outputs for kubernetes provider authentication
    
    * Add sensitive = true to sensitive outputs and update descriptions.
    
    Co-authored-by: Brot & Games <43862266+brotandgames@users.noreply.github.com>

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,6 @@
 output "this" {
   description = "List of node objects"
-  value = [
+  value       = [
     for index, node in var.nodes :
     merge(
       node,
@@ -8,3 +8,32 @@ output "this" {
     )
   ]
 }
+
+output "kube_config_yaml_path" {
+  description = "Path of the kube_config_yaml file"
+  value       = local_file.kube_cluster_yaml.filename
+}
+
+output "api_server_url" {
+  description = "RKE k8s cluster api server url "
+  value       = rke_cluster.this.api_server_url
+}
+
+output "client_cert" {
+  description = "RKE k8s cluster client certificate"
+  value       = rke_cluster.this.client_cert
+  sensitive   = true
+}
+
+output "client_key" {
+  description = "RKE k8s cluster client key"
+  value       = rke_cluster.this.client_key
+  sensitive   = true
+}
+
+output "ca_cert" {
+  description = "RKE k8s cluster CA certificate"
+  value = rke_cluster.this.ca_crt
+  sensitive   = true
+}
+
